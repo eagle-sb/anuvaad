998c03ee26fc20cfbaa27f4b631381b3f3fc8ebb
jainy <jainyjoy97@gmail.com>
2020-10-28 12:37:36 +0530

    change in config file

--- anuvaad-api/anuvaad-user-management/user-management/user-management/app.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.blueprints import Blueprint
 from flask_cors import CORS
-from anuvaad_auditor.loghandler import log_info ,log_exception
+from anuvaad_auditor.loghandler import log_info, log_exception
 from anuvaad_auditor.errorhandler import post_error
 import routes
 import config
@@ -11,44 +11,43 @@ import requests
 import json
 
 
-role_codes_filepath=config.ROLE_CODES_URL
-json_file_loc = "/home/jainy/Documents/usrmgmt"
-json_file_path_delimiter = "/"
-json_file_name = "roles.json"
-
+role_codes_filepath = config.ROLE_CODES_URL
+json_file_dir =  config.ROLE_CODES_DIR_PATH                               #"/home/jainy/Documents/usrmgmt/"
+json_file_name = config.ROLE_CODES_FILE_NAME
 
 
 def read_role_codes():
     try:
         file = requests.get(role_codes_filepath, allow_redirects=True)
-        file_path = json_file_loc + json_file_path_delimiter + json_file_name
+        file_path = json_file_dir + json_file_name
         open(file_path, 'wb').write(file.content)
         with open(file_path, 'r') as stream:
             parsed = json.load(stream)
             roles = parsed['roles']
-            rolecodes=[]
+            rolecodes = []
             for role in roles:
                 rolecodes.append(role["code"])
             return rolecodes
     except Exception as exc:
-        log_exception("Exception while reading configs: " + str(exc), None, exc)
-        post_error("CONFIG_READ_ERROR", "Exception while reading configs: " + str(exc), None)
+        log_exception("Exception while reading configs: " +
+                      str(exc), None, exc)
+        post_error("CONFIG_READ_ERROR",
+                   "Exception while reading configs: " + str(exc), None)
+
 
 # global ROLE_CODES
-ROLE_CODES =read_role_codes()
+ROLE_CODES = read_role_codes()
 
-server  = Flask(__name__)
+server = Flask(__name__)
 
 if config.ENABLE_CORS:
-    cors    = CORS(server, resources={r"/api/*": {"origins": "*"}})
+    cors = CORS(server, resources={r"/api/*": {"origins": "*"}})
 
 for blueprint in vars(routes).values():
     if isinstance(blueprint, Blueprint):
         server.register_blueprint(blueprint, url_prefix=config.CONTEXT_PATH)
 
 if __name__ == "__main__":
-    log_info('starting server at {} at port {}'.format(config.HOST, config.PORT), MODULE_CONTEXT)
+    log_info('starting server at {} at port {}'.format(
+        config.HOST, config.PORT), MODULE_CONTEXT)
     server.run(host=config.HOST, port=config.PORT, debug=config.DEBUG)
-    
-
-
--- anuvaad-api/anuvaad-user-management/user-management/user-management/config.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/config.py
@@ -19,4 +19,8 @@ USR_TOKEN_MONGO_COLLECTION = os.environ.get(
 
 MIN_LENGTH = os.environ.get('UMS_PASSWORD_MIN_LENGTH', 6)
 
-ROLE_CODES_URL = os.environ.get('USM_ROLE_CODES_URL','https://raw.githubusercontent.com/project-anuvaad/anuvaad/zuul_gateway/anuvaad-api/anuvaad-zuul-api-gw/dev-configs/roles.json')
+ROLE_CODES_URL = os.environ.get('UMS_ROLE_CODES_URL','https://raw.githubusercontent.com/project-anuvaad/anuvaad/zuul_gateway/anuvaad-api/anuvaad-zuul-api-gw/dev-configs/roles.json')
+
+ROLE_CODES_DIR_PATH=os.environ.get('UMS_ROLE_DIR_PATH','/app/config/')
+
+ROLE_CODES_FILE_NAME=os.environ.get('UMS_FILE_NAME','roles.json')
