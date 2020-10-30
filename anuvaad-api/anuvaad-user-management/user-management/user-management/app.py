89d43400890427196393a8aa3ac4bfa34e451b17
jainy <jainyjoy97@gmail.com>
2020-10-30 13:53:10 +0530

    change in role code validation function

--- anuvaad-api/anuvaad-user-management/user-management/user-management/app.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/app.py
@@ -14,37 +14,40 @@ import json
 role_codes_filepath = config.ROLE_CODES_URL
 json_file_dir = config.ROLE_CODES_DIR_PATH  # "/home/jainy/Documents/usrmgmt/"
 json_file_name = config.ROLE_CODES_FILE_NAME
-
-
-def read_role_codes():
-    try:
-        file = requests.get(role_codes_filepath, allow_redirects=True)
-        file_path = json_file_dir + json_file_name
-        open(file_path, 'wb').write(file.content)
-        log_info("data read from git and pushed to local", MODULE_CONTEXT)
-        with open(file_path, 'r') as stream:
-            parsed = json.load(stream)
-            roles = parsed['roles']
-            log_info("roles read from json are {}".format(
-                roles), MODULE_CONTEXT)
-            rolecodes = []
-            for role in roles:
-                if role["active"]:
-                    rolecodes.append(role["code"])
-                    log_info(
-                "rolecodes read from json is stored on to rolecodes array:{} ".format(rolecodes), MODULE_CONTEXT)
-            return rolecodes
-    except Exception as exc:
-        log_exception("Exception while reading configs: " +
-                      str(exc), MODULE_CONTEXT, exc)
-        post_error("CONFIG_READ_ERROR",
-                   "Exception while reading configs: " + str(exc), MODULE_CONTEXT)
-
-
-# global ROLE_CODES
 ROLE_CODES=[]
 
 server = Flask(__name__)
+
+def read_role_codes():
+    with server.test_request_context():
+        try:
+            file = requests.get(role_codes_filepath, allow_redirects=True)
+            file_path = json_file_dir + json_file_name
+            open(file_path, 'wb').write(file.content)
+            log_info("data read from git and pushed to local", MODULE_CONTEXT)
+            with open(file_path, 'r') as stream:
+                parsed = json.load(stream)
+                roles = parsed['roles']
+                log_info("roles read from json are {}".format(
+                    roles), MODULE_CONTEXT)
+                rolecodes = []
+                for role in roles:
+                    if role["active"]:
+                        rolecodes.append(role["code"])
+                        log_info(
+                    "rolecodes read from json is stored on to rolecodes array:{} ".format(rolecodes), MODULE_CONTEXT)
+            ROLE_CODES=rolecodes
+        except Exception as exc:
+            log_exception("Exception while reading configs: " +
+                        str(exc), MODULE_CONTEXT, exc)
+            post_error("CONFIG_READ_ERROR",
+                    "Exception while reading configs: " + str(exc), MODULE_CONTEXT)
+
+
+# global ROLE_CODES
+
+
+
 
 if config.ENABLE_CORS:
     cors = CORS(server, resources={r"/api/*": {"origins": "*"}})
@@ -56,5 +59,5 @@ for blueprint in vars(routes).values():
 if __name__ == "__main__":
     log_info('starting server at {} at port {}'.format(
         config.HOST, config.PORT), MODULE_CONTEXT)
-    ROLE_CODES=read_role_codes()
+    read_role_codes()
     server.run(host=config.HOST, port=config.PORT, debug=config.DEBUG)
