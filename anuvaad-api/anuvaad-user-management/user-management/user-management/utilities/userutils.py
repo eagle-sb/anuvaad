54799e1f0957e45ab4d6c12016f8c37651fdba65
mahulivishal <vishalmahuli8@gmail.com>
2021-01-20 13:22:36 +0530

    updated email validation regex

--- anuvaad-api/anuvaad-user-management/user-management/user-management/utilities/userutils.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/utilities/userutils.py
@@ -35,7 +35,7 @@ class UserUtils:
 #email validation
     @staticmethod
     def validate_email(email):
-        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        regex = '([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)'
         if (re.search(regex, email)):
             return True
         else:
