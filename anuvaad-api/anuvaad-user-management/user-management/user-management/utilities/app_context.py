cd7e64e73642740859b6aaf532bd0eaa12a55c69
JainyJoy <jainyjoy97@gmail.com>
2021-02-15 22:05:52 +0530

    change in log structure

--- anuvaad-api/anuvaad-user-management/user-management/user-management/utilities/app_context.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/utilities/app_context.py
@@ -1,5 +1,5 @@
 
-MODULE_CONTEXT = {'metadata': {'module': 'USER-MANAGEMENT'}}
+MODULE_CONTEXT = {'metadata': {'module': 'USER-MANAGEMENT','userID':None,'userName':None}}
 
 
 def init():
@@ -7,3 +7,11 @@ def init():
     app_context = {
         'application_context': None
     }
+# class AppContext:
+def addUserID(userID):
+    if userID is not None:
+        MODULE_CONTEXT['metadata']['userID']=userID
+
+def adduserName(userName):
+    if userName is not None:
+        MODULE_CONTEXT['metadata']['userName']=userName
