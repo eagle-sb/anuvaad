b236de6e1e97a971a890b10008bbd92976e30856
Roshan Shah <rosu285shah@gmail.com>
2021-03-01 10:35:12 +0530

    Table level changes

--- anuvaad-fe/anuvaad-webapp/src/ui/containers/web/DocumentDigitization/OcrPageCard.jsx
+++ anuvaad-fe/anuvaad-webapp/src/ui/containers/web/DocumentDigitization/OcrPageCard.jsx
@@ -90,7 +90,33 @@ class OcrPageCard extends React.Component {
         )
     }
 
+    renderTable = (word, region) => {
+        return (
+            <div
+                style={{
+                    position: "absolute",
+                    zIndex: this.action === word.identifier ? 100000 : 2,
+                    color: word.conf < this.props.percent ? 'red' : 'black',
+                    fontSize: word.font && parseInt(Math.ceil(word.font.size + 1)) + 'px',
+                    top: word.boundingBox.vertices[0].y + 'px',
+                    left: word.boundingBox.vertices[0].x - region.boundingBox.vertices[0].x + 'px',
+                    width: word.boundingBox.vertices[1].x - word.boundingBox.vertices[0].x + 'px',
+                }
+                }
+                key={word.identifier}
+                onDoubleClick={() => this.setModalState(word)}
+            >
+                <Textfit mode="single" style={{ width: '100%' }} min={1} max={parseInt(Math.ceil(region.avg_size * 3))} >
+                    {word.text}
+                </Textfit>
+            </div >
+        )
+    }
+
     renderTextSpan = (word, region) => {
+        if (word.class === "CELL") {
+            return word.children.map(wrd => this.renderTable(wrd, region))
+        }
         return (
             <div
                 style={{
